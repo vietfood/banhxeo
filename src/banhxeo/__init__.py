c01233efb16c33085f6844c0eae9499cc265348f
lenguyen1807 <lenguyen18072003@gmail.com>
2025-05-25 23:52:46 +0700

    Update __init__.py

--- src/banhxeo/__init__.py
+++ src/banhxeo/__init__.py
@@ -1,6 +1,7 @@
 import os
 import warnings
 
+
 try:
     import torch
 except ModuleNotFoundError:
@@ -27,13 +28,29 @@ CPU_DEVICE = torch.device("cpu")
 # Set up seed
 DEFAULT_SEED = 1234
 
+# ----------------------
 # Copy from: https://github.com/unslothai/unsloth/blob/main/unsloth/__init__.py
+# Hugging Face Hub faster downloads
+if "HF_HUB_ENABLE_HF_TRANSFER" not in os.environ:
+    os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+pass
+
+# XET is slower in Colab - investigate why
+keynames = "\n" + "\n".join(os.environ.keys())
+if "HF_XET_HIGH_PERFORMANCE" not in os.environ:
+    os.environ["HF_XET_HIGH_PERFORMANCE"] = "1"
+pass
+if "\nCOLAB_" in keynames:
+    os.environ["HF_XET_CHUNK_CACHE_SIZE_BYTES"] = "0"
+pass
+
 # Reduce VRAM usage by reducing fragmentation
 # And optimize pinning of memory
 if GPU_DEVICE == "cuda":
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = (
         "expandable_segments:True,roundup_power2_divisions:[32:256,64:128,256:64,>:32]"
     )
+# ----------------------
 
 # Install pretty traceback
 try:
@@ -44,5 +61,3 @@ except ModuleNotFoundError:
     warnings.warn(
         "Banhxeo: You can install `rich` library to get prettier console/notebook output"
     )
-
-# Some other things
