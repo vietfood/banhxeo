4af9708e2f87d6b2624a350fc9042c7d5789230e
lenguyen1807 <lenguyen18072003@gmail.com>
2025-06-30 02:25:54 +0700

    refactor: delete all old files

--- src/banhxeo/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tokenizer import NLTKTokenizer, Tokenizer, TokenizerConfig
-from .vocabulary import VocabConfig, Vocabulary
-
-
-__all__ = ["NLTKTokenizer", "Tokenizer", "TokenizerConfig", "VocabConfig", "Vocabulary"]
--- src/banhxeo/data/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import BaseTextDataset
-from .dataset.amazon_review import AmazonReviewFullDataset
-from .dataset.imdb import IMDBDataset
-
-
-__all__ = ["BaseTextDataset", "IMDBDataset", "AmazonReviewFullDataset"]
