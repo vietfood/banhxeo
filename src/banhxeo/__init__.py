35dea57369e0eed452a2119857515275c190b69b
lenguyen1807 <lenguyen18072003@gmail.com>
2025-06-30 02:25:29 +0700

    feat: move to Jax/Flax framework, update to python3.10

--- src/banhxeo/__init__.py
+++ src/banhxeo/__init__.py
@@ -1,34 +1,18 @@
 import os
-import warnings
 
+import tensorflow as tf
 
-try:
-    import torch
-except ModuleNotFoundError:
-    raise ImportError(
-        "Banhxeo: Pytorch is not installed.\n"
-        "Please go to https://pytorch.org/.\n to install Pytorch"
-    )
+# https://docs.jax.dev/en/latest/gpu_performance_tips.html
+os.environ["XLA_FLAGS"] = (
+    "--xla_gpu_triton_gemm_any=True " "--xla_gpu_enable_latency_hiding_scheduler=true "
+)
 
-
-# Set up device
-def set_default_gpu():
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    elif torch.backends.mps.is_available():
-        device = torch.device("mps")
-    else:
-        return None
-    return device
-
-
-GPU_DEVICE = set_default_gpu()
-CPU_DEVICE = torch.device("cpu")
+# Ensure TF does not see GPU and grab all GPU memory.
+tf.config.set_visible_devices([], device_type="GPU")
 
 # Set up seed
 DEFAULT_SEED = 1234
 
-# ----------------------
 # Copy from: https://github.com/unslothai/unsloth/blob/main/unsloth/__init__.py
 # Hugging Face Hub faster downloads
 if "HF_HUB_ENABLE_HF_TRANSFER" not in os.environ:
@@ -43,21 +27,3 @@ pass
 if "\nCOLAB_" in keynames:
     os.environ["HF_XET_CHUNK_CACHE_SIZE_BYTES"] = "0"
 pass
-
-# Reduce VRAM usage by reducing fragmentation
-# And optimize pinning of memory
-if GPU_DEVICE == "cuda":
-    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = (
-        "expandable_segments:True,roundup_power2_divisions:[32:256,64:128,256:64,>:32]"
-    )
-# ----------------------
-
-# Install pretty traceback
-try:
-    from rich.traceback import install
-
-    install(show_locals=True, max_frames=20)
-except ModuleNotFoundError:
-    warnings.warn(
-        "Banhxeo: You can install `rich` library to get prettier console/notebook output"
-    )
