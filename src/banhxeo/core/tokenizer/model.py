0859e9d23b861beee671d773a131fd92415fe124
lenguyen1807 <lenguyen18072003@gmail.com>
2025-07-21 22:53:54 +0700

    add  WordLevelModel and fix BPE

--- src/banhxeo/core/tokenizer/model.py
+++ src/banhxeo/core/tokenizer/model.py
@@ -1,21 +1,107 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import Dict
+from collections import defaultdict
+from typing import Iterable, List
 
-from banhxeo.core.tokenizer import Token
+from banhxeo.core.tokenizer import SpecialTokens, Token
+from banhxeo.core.tokenizer.bpe import BPEModel
 from banhxeo.core.tokenizer.pre_tokenizer import PreTokenizedString
+from banhxeo.utils import progress_bar
+from banhxeo.utils.file import default_logger
 
 
 class TokenizerModel(ABC):
     @abstractmethod
-    def tokenize(self, pre_tokenized_str: PreTokenizedString):
-        """
-        Populates tokens for each split in the PreTokenizedString, in-place.
-        """
+    def tokenize(self, pre_tokenized_str: PreTokenizedString): ...
+
+    @abstractmethod
+    def detokenize(self, token_ids: List[int]) -> List[str]: ...
+
+    @abstractmethod
+    def train(
+        self,
+        corpus: Iterable[PreTokenizedString],
+        progress: bool = True,
+        **kwargs,
+    ): ...
+
+    @abstractmethod
+    @classmethod
+    def from_config(cls, config): ...
 
 
 class WordLevelModel(TokenizerModel):
-    """
-    TODO: Implement this
-    """
+    def __init__(self, special_tokens: SpecialTokens):
+        self.special_tokens = special_tokens
+        self.vocab = defaultdict(int)  # token to id
+        self.inverse_vocab = []
+
+        self.trained = False
+
+    def tokenize(self, pre_tokenized_str: PreTokenizedString):
+        if not self.vocab:
+            raise ValueError(
+                "You haven't build Tokenizer. Please use `WordLevelModel.train(...)`"
+            )
+
+        for split in pre_tokenized_str.splits:
+            vocab_id = self.vocab.get(split.normalized.normalized)
+            if vocab_id is None:
+                vocab_id = self.vocab[self.special_tokens.unk_tok]
+
+            split.tokens = [
+                Token(
+                    id=vocab_id,
+                    value=split.normalized.normalized,
+                    offsets=(
+                        split.normalized.alignments[0][0],
+                        split.normalized.alignments[-1][1],
+                    ),
+                )
+            ]
+
+    def detokenize(self, token_ids: List[int]) -> List[str]:
+        return [self.inverse_vocab[token_id] for token_id in token_ids]
+
+    def train(
+        self,
+        corpus: Iterable[PreTokenizedString],  # already in unique word
+        progress: bool = True,
+        **kwargs,
+    ):
+        if self.trained:
+            default_logger.warning("This Tokenized has been trained before. Return")
+            return
+
+        # Add special tokens first
+        self.vocab = {
+            token: idx for idx, token in enumerate(self.special_tokens.special_tokens())
+        }
+
+        self.inverse_vocab = [token for token in self.special_tokens.special_tokens()]
+
+        # Add rest of vocab
+        all_words = set(
+            [
+                split.normalized.normalized
+                for sentence in corpus
+                for split in sentence.splits
+            ]
+        )
+
+        current_id = len(self.vocab)
+        for word in progress_bar(sorted(list(all_words)), disable=not progress):
+            if word not in self.vocab:
+                self.vocab[word] = current_id
+                self.inverse_vocab.append(word)
+                current_id += 1
+
+        self.trained = True
+
+    @classmethod
+    def from_config(cls, config):
+        pass
+
+
+MODEL_FACTORY = {"word": WordLevelModel, "bpe": BPEModel}
